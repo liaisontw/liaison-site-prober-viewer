2d90bf092f906c81126af2cd140d0132c5353b53
liaisontw <liaison.tw@gmail.com>
2026-01-15 17:18:01 +0800

    Updated install-wp-tests.sh with curl not svn.

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -29,19 +29,21 @@ fi
 download "$WP_TARBALL" /tmp/wordpress.tar.gz
 tar -xzf /tmp/wordpress.tar.gz --strip-components=1 -C "$WP_CORE_DIR"
 
-echo "### Download WP test suite (NO SVN)"
+
+echo "### Download WP test suite (GitHub mirror)"
 
 mkdir -p "$WP_TESTS_DIR"
 
-download "https://develop.svn.wordpress.org/${TESTS_TAG}/tests/phpunit/includes.zip" /tmp/includes.zip
-download "https://develop.svn.wordpress.org/${TESTS_TAG}/tests/phpunit/data.zip" /tmp/data.zip
 
-unzip -q /tmp/includes.zip -d "$WP_TESTS_DIR"
-unzip -q /tmp/data.zip -d "$WP_TESTS_DIR"
+TESTS_REPO="https://github.com/WordPress/wordpress-develop/archive/refs/heads/trunk.zip"
 
-download \
-  "https://develop.svn.wordpress.org/${TESTS_TAG}/wp-tests-config-sample.php" \
-  "$WP_TESTS_DIR/wp-tests-config.php"
+download "$TESTS_REPO" /tmp/wp-tests.zip
+unzip -q /tmp/wp-tests.zip -d /tmp/
+
+cp -R /tmp/wordpress-develop-trunk/tests/phpunit/includes "$WP_TESTS_DIR/"
+cp -R /tmp/wordpress-develop-trunk/tests/phpunit/data "$WP_TESTS_DIR/"
+cp /tmp/wordpress-develop-trunk/wp-tests-config-sample.php \
+   "$WP_TESTS_DIR/wp-tests-config.php"
 
 sed -i "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR/wp-tests-config.php"
 sed -i "s/youremptytestdbnamehere/$DB_NAME/" "$WP_TESTS_DIR/wp-tests-config.php"
